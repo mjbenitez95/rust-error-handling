83506018373f55f325042e989bb0ec401dc87a1b
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-29 07:47:24 -0800

    print panic statements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 fn main() {
     panic_statement();
     panic_in_library();
-    
 }
 
 fn panic_statement() {
-    panic!("We're crashing and burning!");
+    println!(
+        "Panic statement:
+        \"panic!(\"We're crashing and burning!\")."
+    );
 }
 
 fn panic_in_library() {
-    let v = vec![1, 2, 3];
-    v[99];
+    println!(
+        "Would cause panic in library: 
+        \"
+          let v = vec![1, 2, 3];
+          v[99];
+        \""
+    );
 }

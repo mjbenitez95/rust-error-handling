a9692e8e1ed7f38308d1ebf4d7ddf1d32434a227
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-29 08:26:43 -0800

    add second example of file not found error handling

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn panic_in_library() {
 fn recover_with_result() {
     let f = File::open("hello.txt");
 
-    let f = match f {
+    let _f = match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
@@ -35,8 +35,18 @@ fn recover_with_result() {
                 Err(e) => panic!("Problem creating the file {:?}", e),
             },
             other_error => {
-                panic!("Problem opening the file: {:?}", error)
+                panic!("Problem opening the file: {:?}", other_error)
             }
         },
     };
+
+    let _g = File::open("goodbye.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 }

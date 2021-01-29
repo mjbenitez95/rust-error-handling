d493abd3a20ca8681f676bfa72546de1e597ce82
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-29 07:43:24 -0800

    add panic_in_library() and restructure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
-    // example of panic statement
+    panic_statement();
+    panic_in_library();
+    
+}
+
+fn panic_statement() {
     panic!("We're crashing and burning!");
+}
+
+fn panic_in_library() {
+    let v = vec![1, 2, 3];
+    v[99];
 }
